f768746300917d5c7d9dc3d845afe5381e4b1c47
Jason Yeung <contact@jasonyeung.me>
2020-12-02 22:58:14 -0800

    finish day 3

---
+++ day_3/part_1.js
@@ -0,0 +1,47 @@
+/*
+https://adventofcode.com/2020/day/3
+node day_3/part_1.js
+*/
+const PATH = './input.txt'
+
+const path = require("path")
+const fs = require('fs')
+let input = fs.readFileSync(path.resolve(__dirname, PATH))
+              .toString()
+              .split('\r\n')
+
+/*
+ THOUGHT PROCCESS
+ ----------------
+ the tobaggan goes towards a slope of (3 RIGHTS, 1 DOWN)
+ so as a result, we should keep track of the tobaggan's position every 3 steps
+ while moving down the map 1 row at a time, checking whether we hit a tree:
+
+ start with tobaggan's pos at 0
+
+ for rows from m = 1 to bottom row
+  ⚠ we modulo the position because the map wraps around at the end
+  tobaggan's pos = (tobbagan's pos + 3) % (# of columns)
+
+  is (rows[m].charAt(tobbagan's pos) == '#')? (aka, is 🎄?)
+    if it is, increment tree count
+
+ return tree count
+*/
+
+const map = input
+const num_cols = map[0].length
+
+let trees = 0
+let tobaggan_pos = 0
+
+for (let m = 1; m < map.length; m += 1) {
+  tobaggan_pos = (tobaggan_pos + 3) % num_cols
+
+  if (map[m].charAt(tobaggan_pos) === '#')
+    trees++
+}
+
+console.log(trees)
+
+
---
+++ day_3/part_2.js
@@ -0,0 +1,51 @@
+/*
+https://adventofcode.com/2020/day/3
+node day_3/part_1.js
+*/
+const PATH = './input.txt'
+
+const path = require("path")
+const fs = require('fs')
+let input = fs.readFileSync(path.resolve(__dirname, PATH))
+              .toString()
+              .split('\r\n')
+
+/*
+ THOUGHT PROCCESS
+ ----------------
+ Given my reasoning in part 1, we can abstract this
+ to apply for any given slopes:
+ 
+ Assuming we have a slope (N RIGHTS, M DOWNS), 
+ we should keep track of the tobaggan's position every N step(s)
+ while moving down the map M row(s) at a time, checking whether we hit a tree
+*/
+
+function getTreeHits(rights, downs) {
+  const map = input
+  const num_cols = map[0].length
+
+  let trees = 0
+  let tobaggan_pos = 0
+
+  for (let m = downs; m < map.length; m += downs) {
+    tobaggan_pos = (tobaggan_pos + rights) % num_cols
+
+    if (map[m].charAt(tobaggan_pos) === '#')
+      trees++
+  }
+
+  return trees
+}
+
+console.log(
+  getTreeHits(1, 1) *
+  getTreeHits(3, 1) *
+  getTreeHits(5, 1) *
+  getTreeHits(7, 1) *
+  getTreeHits(1, 2)
+)
+
+
+
+
